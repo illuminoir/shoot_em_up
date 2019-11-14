a076b4e1fb7a89e74c8dc2e1760661bd82706c09
kdufour <kdufour@etud.u-pem.fr>
2019-11-14 18:04:11 +0100

    add structure and function to move enemy

--- include/enemy.h
+++ include/enemy.h
@@ -1,1 +1,19 @@
+#ifndef __ENEMY__
+#define __ENEMY__
+
 #include "../include/projectiles.h"
+
+typedef struct enemy{
+	int x;
+	int y;
+	int vitesse;
+	Tir *tir;
+}Enemy;
+
+Enemy InitEnemy(int x, int y);
+
+void MoveEnemy(Enemy *enemy);
+
+void TirEnemy(Enemy enemy);
+
+#endif
