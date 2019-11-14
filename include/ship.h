11c815a78c8be8d962d128b3455f4252519ea8eb
Allan K <allan_77@live.fr>
2019-11-14 15:08:00 +0100

    ship initialisation
    creation of the Ship struct, function to initialise a Ship object

--- include/ship.h
+++ include/ship.h
@@ -1,1 +1,27 @@
+#ifndef __SHIP__
+#define __SHIP__
+
 #include "../include/projectiles.h"
+
+#define IDLE 0
+#define MOVING_UP 1
+#define MOVING_DOWN 2
+/* #define SPRITE_SIZE ?? */
+
+typedef struct _ship {
+	int x;
+	int y;
+	int health;
+	int current_sprite; /* gives the sprite index depending on the pressed key */
+	int speed; /* speed vector */
+	int has_missile; /* missle upgrade */
+	int has_double; /* double shot upgrade */
+	int has_laser; /* laser upgrade */
+	int has_option; /* option upgrade */
+	/*
+	Tir* projectiles; */ /* ship's projectiles */
+}Ship;
+
+void init_ship(Ship* ship);
+
+#endif
--- src/ship.c
+++ src/ship.c
@@ -1,1 +1,22 @@
 #include "../include/ship.h"
+
+
+/* --------------------------- */
+void init_ship(Ship* ship)
+/* --------------------------- */
+{
+	ship->x = WINDOW_SIZE_X / 20;
+	ship->y = (WINDOW_SIZE_Y / 2) /*- (SPRITE_SIZE / 2)*/; /* in the middle of the screen */
+
+	ship->health = 100;
+	ship->current_sprite = IDLE;
+	ship->speed = 1; /* added to the ship's position to make it move */
+
+	ship->has_missile = 0;
+	ship->has_double = 0;
+	ship->has_laser = 0;
+	ship->has_option = 0;
+/*
+	projectiles = (Tir *)malloc(sizeof(Tir) * 100);
+*/
+}
