a1100f7d5e2dce6c3a38d025756487ddf80b909c
kdufour <kdufour@etud.u-pem.fr>
2019-11-14 17:58:55 +0100

    add function to move enemy

--- src/enemy.c
+++ src/enemy.c
@@ -1,1 +1,28 @@
 #include "../include/enemy.h"
+#include <assert.h>
+
+
+Enemy InitEnemy(int x, int y){
+	Enemy enemy;
+	enemy.x = x;
+	enemy.y = y;
+	/*enemy.tir = ;*/
+	enemy.vitesse = 1;
+	return enemy;
+}
+
+void MoveEnemy(Enemy *enemy){
+	assert(enemy != NULL);
+
+	while((enemy->x) >= 0){
+		enemy->x -= enemy->vitesse;
+	}
+}
+
+void TirEnemy(Enemy enemy){
+
+}
+
+void AddVitesse(Enemy *enemy, int vitesse){
+	enemy->vitesse = vitesse;
+}
